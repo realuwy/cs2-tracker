6cca99adb0549c333359d7cc961e9ea999da765a
uwy <101711897+realuwy@users.noreply.github.com>
2025-10-10 23:23:37 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // scan all relevant files inside /src
+  ],
   theme: {
     extend: {},
   },
